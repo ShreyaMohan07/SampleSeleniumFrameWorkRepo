27879fea4776924cae921c6fc743334e80a3f07c
HP <HP@DESKTOP-TPUJN1N>
2025-02-27 18:01:36 +0530

    added new POM class

---
+++ SampleSeleniumFrameWork/src/main/java/com/comcast/crm/objectrepository/ProductsPage.java
@@ -0,0 +1,9 @@
+package com.comcast.crm.objectrepository;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductsPage {
+@FindBy(xpath="//img[@alt='Create Product...']")
+private WebElement createproductImgBtn;
+}
